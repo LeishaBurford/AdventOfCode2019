921e249ccd2c4a0f3913f60293a367b59d888a52
LeishaBurford <leishabobisha@gmail.com>
2019-12-09 20:30:05 -0500

    Day 3 puzzle 1 done

--- Day3.py
+++ Day3.py
@@ -1,52 +1,49 @@
 from pathlib import Path
 import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+
 data_folder = Path("./PuzzleData/")
-
 file_to_open = data_folder / "Day3.txt"
-
 
 with open(file_to_open) as input:
     wireMoves = [wire.strip().split(',') for wire in input.readlines()]
-print(wireMoves)
 
 def getNextPositions(move, currentPosition):
     if move[0] =='R':
         lineEnd = [currentPosition[0] + int(move[1:]), currentPosition[1]]
-        return np.array([[x, lineEnd[1]] for x in range(currentPosition[0], lineEnd[0] + 1)]).flatten()
+        return [[x, lineEnd[1]] for x in range(currentPosition[0] + 1, lineEnd[0] + 1)]
     if move[0] =='L':
         lineEnd = [currentPosition[0] - int(move[1:]), currentPosition[1]]
-        return np.array([[x, lineEnd[1]] for x in range(currentPosition[0], lineEnd[0] + 1, -1)]).flatten()
+        return [[x, lineEnd[1]] for x in range(currentPosition[0] - 1, lineEnd[0] - 1, -1)]
     if move[0] =='U':
         lineEnd = [currentPosition[0], currentPosition[1] + int(move[1:])]  
-        return np.array([[lineEnd[0], x] for x in range(currentPosition[1], lineEnd[1] + 1)]).flatten()
+        return [[lineEnd[0], x] for x in range(currentPosition[1] + 1, lineEnd[1] + 1)]
     if move[0] =='D':
         lineEnd = [currentPosition[0], currentPosition[1] - int(move[1:])]
-        return np.array([[lineEnd[0], x] for x in range(currentPosition[1], lineEnd[1] + 1, -1)]).flatten()
+        return [[lineEnd[0], x] for x in range(currentPosition[1] - 1, lineEnd[1] - 1, -1)]
 
 def getWirePositions(wire):
     wirePositions = []
     currentPosition = [0, 0]
     for move in wire:
-        # print(getNextPositions(move, currentPosition))
         nextPosition = getNextPositions(move, currentPosition)
         wirePositions.append(nextPosition)
         currentPosition = nextPosition[-1]
-    print(wirePositions)
     return wirePositions
 
 def getManhattanDistance(a, b):
     return abs(a[0] - b[0]) + abs(b[1] - a[1])
 
-wireOnePositions = getWirePositions(wireMoves[0])
-wireTwoPositions = getWirePositions(wireMoves[1])
+wireOnePositions = list(itertools.chain(*getWirePositions(wireMoves[0])))
+wireTwoPositions = list(itertools.chain(*getWirePositions(wireMoves[1])))
 
-print(wireOnePositions)
-print(wireTwoPositions)
+# Graph it for fun :)
+plt.scatter(*zip(*wireOnePositions))
+plt.scatter(*zip(*wireTwoPositions))
+plt.show()
 
-intersections = [point for point in wireOnePositions if [point[0], point[1]] in wireTwoPositions]
-print(intersections)
+intersections =  set([(point[0], point[1]) for point in wireOnePositions ]).intersection(set([(point[0], point[1]) for point in wireTwoPositions ]))
 distances = [getManhattanDistance([0, 0], point) for point in intersections]
 
-
-
-print(max(distances))
+print(min(distances))
