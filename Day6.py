dd9abf7c3443f0ec7458e70f0da8dc50617ac564
LeishaBurford <leishabobisha@gmail.com>
2019-12-11 21:01:59 -0500

    day 6 done

--- Day6.py
+++ Day6.py
@@ -5,13 +5,33 @@ file_to_open = data_folder / "Day6.txt"
 with open(file_to_open) as orbitData:
     orbits = [orbit.strip().split(')') for orbit in orbitData.readlines()]
 
-orbits = [[orbit[1], orbit[0]] for orbit in orbits]
-# make into chain of dict
-# print(orbits)
+orbits = {orbit[1]: orbit[0] for orbit in orbits}
+
+def getOrbitCount(startingOrbit, recordedOrbits):
+    count = 0
+    while startingOrbit != 'COM':
+        count += 1
+        startingOrbit = orbits[startingOrbit]
+    return count
+
+def getPathToCOM(startingOrbit, recordedOrbits):
+    orbitPath = []
+    while startingOrbit != 'COM':
+        orbitPath.append(startingOrbit)
+        startingOrbit = orbits[startingOrbit]
+    return orbitPath
 
 orbitCount = 0
-currentOrbit = orbits[-1][1]
-while currentOrbit != 'COM':
-    # use dict key lookup to find current orbit
-    # update count and current orbit
-    # https://dbader.org/blog/python-dictionaries-maps-and-hashtables
+for orbit in orbits.keys():
+    currentOrbit = orbit
+    orbitCount += getOrbitCount(currentOrbit, orbits)
+
+print('Part 1: ', orbitCount)
+
+myPath = getPathToCOM(orbits['YOU'], orbits)
+santaPath = getPathToCOM(orbits['SAN'], orbits)
+intersection = [me for me in myPath if me in santaPath]
+minimumOrbitCount = len(myPath) + len(santaPath) - 2 * len(intersection)
+
+print('Part 2: ',minimumOrbitCount)
+
