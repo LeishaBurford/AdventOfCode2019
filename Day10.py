4c3041fab665f8c3a2683969941029673d1ad30c
Leisha Burford <lburford@brocksolutions.com>
2019-12-18 13:08:18 -0500

    day 10 part 1 done

---
+++ Day10.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+data_folder = Path("./PuzzleData/")
+file_to_open = data_folder / "Day10.txt"
+with open(file_to_open) as input:
+    asteroids = [[asteroid for asteroid in row.strip()]
+                 for row in input.readlines()]
+
+
+def prettyPrintMatrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+prettyPrintMatrix(asteroids)
+
+
+def getAsteroidLocations(asteroidLocations, position):
+    postions = []
+    for x, row in enumerate(asteroidLocations):
+        for y, col in enumerate(row):
+            if col == '#':
+                postions.append((x + position[0], y + position[1]))
+    return postions
+
+
+def getQuadrants(asteroidLocations, stationLocation):
+    a = stationLocation[0]
+    b = stationLocation[1]
+    firstQuadrant = [[location for location in row[:b]]
+                     for row in asteroidLocations[:a]]
+    firstQuadrant = getAsteroidLocations(firstQuadrant, (0, 0))
+    secondQuadrant = [[location for location in row[b + 1:]]
+                      for row in asteroidLocations[:a]]
+    secondQuadrant = getAsteroidLocations(secondQuadrant, (0, b + 1))
+    thirdQuadrant = [[location for location in row[:b]]
+                     for row in asteroidLocations[a + 1:]]
+    thirdQuadrant = getAsteroidLocations(thirdQuadrant, (a + 1, 0))
+    fourthQuadrant = [[location for location in row[b + 1:]]
+                      for row in asteroidLocations[a + 1:]]
+    fourthQuadrant = getAsteroidLocations(fourthQuadrant, (a + 1, b + 1))
+
+    return [firstQuadrant, secondQuadrant, thirdQuadrant, fourthQuadrant]
+
+
+def getDirectAsteroids(asteroidLocations, stationLocation):
+    total = 0
+    a = stationLocation[0]
+    b = stationLocation[1]
+    if asteroidLocations[b][:a].count('#') > 0:
+        total += 1
+    if asteroidLocations[b][a + 1:].count('#') > 0:
+        total += 1
+
+    verticalLineOfSight = [location[a] for location in asteroids]
+    if verticalLineOfSight[:b].count('#') > 0:
+        total += 1
+    if verticalLineOfSight[b + 1:].count('#') > 0:
+        total += 1
+    return total
+
+def getSlope(a, b):
+    return (a[0] - b[0]) / (a[1] - b[1])
+
+
+possibleStationLocations = []
+for x, row in enumerate(asteroids):
+    for y, col in enumerate(row):
+        if asteroids[x][y] == '#':
+            possibleStationLocations.append((x, y))
+
+bestAsteroidVisibility = ((0, 0), 0)
+for position in possibleStationLocations:
+    asteroidCount = getDirectAsteroids(asteroids, position)
+    for quad in getQuadrants(asteroids, position):
+        slopes = []
+        for location in quad:
+            if location != position:
+                slope = getSlope(position, location)
+                if slope not in slopes:
+                    slopes.append(slope)
+        asteroidCount += len(slopes)
+    # print((position, asteroidCount))
+    if asteroidCount > bestAsteroidVisibility[1]:
+        bestAsteroidVisibility = (position, asteroidCount)
+
+print(bestAsteroidVisibility)
