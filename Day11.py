bd72a558ebcbc36008273f0aec4b09e229f96c04
LeishaBurford <leishabobisha@gmail.com>
2019-12-21 16:56:39 -0500

    helps to not keep resetting the relative base

--- Day11.py
+++ Day11.py
@@ -1,6 +1,5 @@
-# import itertools
+import itertools
 from pathlib import Path
-# from collections import deque
 
 data_folder = Path("./PuzzleData/")
 file_to_open = data_folder / "Day11.txt"
@@ -8,7 +7,6 @@ with open(file_to_open) as instructionInput:
     opCodes = [int(code) for code in instructionInput.read().split(',')]
 
 originalProgram = [code for code in opCodes]
-
 
 def getParameter(instructions, index, parameterCode, relativeBase):
     parameterValue = 0
@@ -20,13 +18,14 @@ def getParameter(instructions, index, parameterCode, relativeBase):
         parameterValue = relativeBase + instructions[index]
     for i in range(len(instructions), parameterValue + 1):
         instructions.append(0)
+    if parameterValue == -1:
+        print('ahhhhh')
     return parameterValue
 
-
-def compute(instructions, inputSignal, currentIndex, outputSignals):
+def compute(instructions, inputSignal, currentIndex, outputSignals, relativeBase):
     index = currentIndex
     programStatus = 'CONTINUE'
-    relativeBase = 0
+    # relativeBase = 0
     needSecondParam = [1, 2, 5, 6, 7, 8]
     needThirdParam = [1, 2, 7, 8]
     while index < len(instructions):
@@ -35,12 +34,9 @@ def compute(instructions, inputSignal, currentIndex, outputSignals):
             programStatus = 'HALT'
             break
         fullCode = [int(digit) for digit in str(instructions[index]).zfill(5)]
-        parameter1 = getParameter(
-            instructions, index + 1, fullCode[2], relativeBase)
-        parameter2 = getParameter(
-            instructions, index + 2, fullCode[1], relativeBase) if opCode in needSecondParam else 0
-        parameter3 = getParameter(
-            instructions, index + 3, fullCode[0], relativeBase) if opCode in needThirdParam else 0
+        parameter1 = getParameter(instructions, index + 1, fullCode[2], relativeBase)
+        parameter2 = getParameter(instructions, index + 2, fullCode[1], relativeBase) if opCode in needSecondParam else 0       
+        parameter3 = getParameter(instructions, index + 3, fullCode[0], relativeBase) if opCode in needThirdParam else 0      
         if opCode == 1:
             instructions[parameter3] = instructions[parameter1] + \
                 instructions[parameter2]
@@ -71,8 +67,7 @@ def compute(instructions, inputSignal, currentIndex, outputSignals):
         if opCode == 9:
             relativeBase += instructions[parameter1]
             index += 2
-    return (instructions, index, programStatus)
-
+    return (instructions, index, programStatus, relativeBase)
 
 def move(position, moveCode):
     if moveCode == 0:
@@ -96,12 +91,13 @@ def getRobotDirection(moveRight, currentDirection):
         return 0 if moveRight else 2
 
 
-def paint(startingColour):
+def paint(startingColour, program):
     halted = False
     robotPosition = (0, 0)
     positions = {robotPosition: startingColour}
     robotDirection = 0
     programPosition = 0
+    relativeBase = 0
     while not halted:
         outputSignals = []
         if robotPosition in positions:
@@ -109,26 +105,48 @@ def paint(startingColour):
         else:
             intputSignal = [0]
         computerResponse = compute(
-            opCodes, intputSignal, programPosition, outputSignals)
+            program, intputSignal, programPosition, outputSignals, relativeBase)
         programPosition = computerResponse[1]
+        relativeBase = computerResponse[3]
         if computerResponse[2] == 'HALT':
-            break
-        positions[robotPosition] = outputSignals[0]
-        robotDirection = getRobotDirection(outputSignals[1], robotDirection)
-        robotPosition = move(robotPosition, robotDirection)
+            halted = True
+        if outputSignals != []:
+            positions[robotPosition] = outputSignals[0]
+            robotDirection = getRobotDirection(outputSignals[1], robotDirection)
+            robotPosition = move(robotPosition, robotDirection)
+            if robotPosition == (1, 0):
+                print('ahhhhh')
     return positions
 
 
-print('Part 1:', len(paint(0)))
+# print('Part 1:', len(paint(0, opCodes)))
 
-paintedPositions = paint(1)
+# reset computer for part 2
+opCodes = originalProgram
+
+paintedPositions = paint(1, opCodes)
+paintedWhite = [position for position in paintedPositions if paintedPositions[position] == 1 ]
+
+invalidPoint = [point for point in paintedPositions if paintedPositions[point] != 1 and paintedPositions[point] != 0]
+
+# xSorted = sorted(paintedWhite, key=lambda x: x[0])
 xSorted = sorted(paintedPositions.keys(), key=lambda x: x[0])
+
+# ySorted = sorted(paintedWhite, key=lambda x: x[1])
 ySorted = sorted(paintedPositions.keys(), key=lambda x: x[1])
 
-for y in range(ySorted[0][1], ySorted[-1][1] + 1):
+for y in range(ySorted[-1][1], ySorted[0][1] - 1, -1):
     for x in range(xSorted[0][0], xSorted[-1][0] + 1):
-        if (x, y) in paintedPositions:
-            print(paintedPositions[(x, y)], end='')
+        if (x, y) in paintedWhite:
+            print('#', end='')
         else:
-            print(' ', '')
+            print(' ', end='')
     print()
+
+# for y in range(-50, 50):
+#     for x in range(-40,40):
+#         if (x, y) in paintedWhite:
+#             print('#', end='')
+#         else:
+#             print(' ', end='')
+#     print()
